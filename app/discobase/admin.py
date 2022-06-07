ea5449e6c824a6975520202fb9c598256dced282
rbuerki <r2d4@bluewin.ch>
2022-06-07 15:17:00 +0200

    Tweak discobase model

--- app/discobase/admin.py
+++ app/discobase/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from discobase.models import (
+    Artist,
+    Country,
+    CreditTrx,
+    Genre,
+    Label,
+    Record,
+    RecordFormat,
+)
+
+
+class RecordAdmin(admin.ModelAdmin):
+    """Customize record list view in the admin panel."""
+
+    model = Record
+    list_display = (
+        "id",
+        "title",
+        "purchase_date",
+        "is_active",
+    )  # TODO: show many to many fields
+    list_display_links = ("id", "title")
+    list_filter = ("genre",)  # TODO how to filter on artist or label ...
+    list_editable = ("purchase_date",)  # more for demo purposes ...
+    search_fields = (
+        "title",
+        "genre",
+        "record_format",
+    )
+    list_per_page = 30
+
+
+admin.site.register(Record, RecordAdmin)
+
+# TODO ...
+admin.site.register([Artist, Country, CreditTrx, Genre, Label, RecordFormat])
--- app/discobase/models.py
+++ app/discobase/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -22,7 +24,7 @@ class Country(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"<id={self.id}, name={self.country_name}>"
+        return self.country_name
 
 
 class Artist(models.Model):
@@ -33,7 +35,7 @@ class Artist(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"<id={self.id}, name={self.artist_name}, country={self.country}>"
+        return f"{self.artist_name} ({self.country})"
 
 
 class Genre(models.Model):
@@ -43,7 +45,7 @@ class Genre(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"<id={self.id}, name={self.genre_name}>"
+        return self.genre_name
 
 
 class Label(models.Model):
@@ -53,7 +55,7 @@ class Label(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"<id={self.id}, name={self.label_name}>"
+        return self.label_name
 
 
 class RecordFormat(models.Model):
@@ -63,26 +65,27 @@ class RecordFormat(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"<id={self.id}, name={self.name}>"
+        return self.format_name
 
 
 class Record(models.Model):
     # id = models.AutoField(primary_key=True)  # TODO: check if UUID is better
     title = models.CharField(max_length=255)
-    year = models.SmallIntegerField()
+    year = models.SmallIntegerField(default=datetime.now().year)
+    record_format = models.OneToOneField(RecordFormat, on_delete=models.CASCADE)
     color = models.CharField(max_length=255, blank=True)
     lim_edition = models.CharField(max_length=50, blank=True)
     number = models.CharField(max_length=50, blank=True)
     remarks = models.CharField(max_length=255, blank=True)
+    genre = models.OneToOneField(Genre, on_delete=models.CASCADE)
+    artist = models.ManyToManyField(Artist)
+    label = models.ManyToManyField(Label)
     purchase_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.SmallIntegerField(validators=[validate_rating_value], null=True)
-    review = models.TextField(blank=True)
     is_digitized = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    genre = models.OneToOneField(Genre, on_delete=models.CASCADE)
-    record_format = models.OneToOneField(RecordFormat, on_delete=models.CASCADE)
-    artist = models.ManyToManyField(Artist)
+    rating = models.SmallIntegerField(validators=[validate_rating_value], null=True)
+    review = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -91,7 +94,7 @@ class Record(models.Model):
     #     return self.id
 
     def __str__(self):
-        return f"<id={self.id}, title={self.title}, artist={self.artists}>"
+        return f"{self.title} ({self.artist})"
 
 
 class CreditTrx(models.Model):
