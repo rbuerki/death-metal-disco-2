198ff2ca4749a02e05228586b2dcaa27c8d55562
rbuerki <r2d4@bluewin.ch>
2022-06-14 18:22:38 +0200

    Add docstrings

--- dev/1_db_migration_sqlite_to_postgres/1-migrate_discobase_from_sqlite_to_pg.py
+++ dev/1_db_migration_sqlite_to_postgres/1-migrate_discobase_from_sqlite_to_pg.py
@@ -15,10 +15,12 @@
 
 # # What
 #
-# First step in this project is to migrate the DB from Sqlite to PostgreSQL. This is not a 1:1 migration, as there will be some tweaks to the schema. 
+# First step in this project is to migrate the DB from Sqlite to PostgreSQL. This is not a 1:1 migration, as there will be some tweaks to the schema.
 #
 # I used two connections and sqlalchemy/pandas for the transfer (and the data manipulation), code is messy, but it worked well.
-
+#
+# Addendum: I had to reset the indexing sequence to max(id)+1 in the db, because Django wanted to start with id 1 ...
+# (see [here](https://stackoverflow.com/a/118402/13797028))
 # +
 import sys
 from datetime import datetime
@@ -34,7 +36,7 @@ import sqlalchemy
 
 print(sys.executable)
 print(sys.version)
-print(f'Pandas {pd.__version__}')
+print(f"Pandas {pd.__version__}")
 
 # +
 # Create connectin string for sqlalchemy or ipython-sql
@@ -143,7 +145,7 @@ distinct_countries = [a for (a,) in distinct_countries]
 full_countries = pd.read_csv("country_list.csv")
 full_countries.head()
 
-all_countries  = full_countries["Name"].to_list()
+all_countries = full_countries["Name"].to_list()
 # -
 
 set(distinct_countries) - set(all_countries)
@@ -168,11 +170,12 @@ SET artist_country = 'United States' WHERE artist_country = 'USA';
 
 df_countries = pd.DataFrame(distinct_countries, columns=["country_name"])
 df_countries = pd.merge(
-    df_countries, full_countries, 
-    how="left", 
-    left_on="country_name", 
+    df_countries,
+    full_countries,
+    how="left",
+    left_on="country_name",
     right_on="Name",
-    validate="one_to_one"
+    validate="one_to_one",
 )
 df_countries.drop(["Name"], axis=1, inplace=True)
 df_countries.fillna("XX", inplace=True)
@@ -184,11 +187,13 @@ assert len(df_countries) == len(distinct_countries)
 df_countries
 
 
-# "Connect" to sqlalchemy 
+# "Connect" to sqlalchemy
 engine = sqlalchemy.create_engine(sqlite_conn_str)
 engine
 
-df_countries.to_sql('countries', engine, index=False, index_label="pk", if_exists="replace")
+df_countries.to_sql(
+    "countries", engine, index=False, index_label="pk", if_exists="replace"
+)
 
 # ### Labels
 #
@@ -206,7 +211,7 @@ label_query = """
     WHERE label_id = 1
 """
 
-label_query_2 ="""
+label_query_2 = """
     SELECT
         label_name
     FROM labels l
@@ -236,15 +241,17 @@ trx_query = """
 #
 
 df_records = pd.read_sql(
-    "SELECT * FROM records", 
-    engine, 
-    coerce_float=True, 
-    parse_dates=["purchase_date", "created_at", "updated_at"]
+    "SELECT * FROM records",
+    engine,
+    coerce_float=True,
+    parse_dates=["purchase_date", "created_at", "updated_at"],
 )
 df_records.head()
 
 df_records = df_records.replace("None", "")
-df_records[["lim_edition", "number", "remarks"]] = df_records[["lim_edition", "number", "remarks"]].fillna("")
+df_records[["lim_edition", "number", "remarks"]] = df_records[
+    ["lim_edition", "number", "remarks"]
+].fillna("")
 
 
 # #### Merge number, lim_edition and remarks
@@ -256,6 +263,7 @@ def numbered(num, lim):
     else:
         return ""
 
+
 def limited(num, lim):
     if len(num) > 0:
         return num
@@ -264,23 +272,26 @@ def limited(num, lim):
     elif (len(lim) > 0) and (lim != "lim"):
         return f" limited: {lim}"
     else:
-        return ""     
-    
+        return ""
+
+
 def remarks(lim, rem):
-    if (len(rem) > 0) and (len(lim) >  0):
+    if (len(rem) > 0) and (len(lim) > 0):
         return lim + ", " + rem
-    elif (len(rem) > 0) and not (len(lim) >  0):
+    elif (len(rem) > 0) and not (len(lim) > 0):
         return rem
     elif (len(lim) > 0) and not (len(rem) > 0):
         return lim
     else:
-        return ""    
+        return ""
 
 
 # +
 df_1 = df_records.copy()
 df_1["number"] = df_1.apply(lambda x: numbered(x["number"], x["lim_edition"]), axis=1)
-df_1["lim_edition"] = df_1.apply(lambda x: limited(x["number"], x["lim_edition"]), axis=1)
+df_1["lim_edition"] = df_1.apply(
+    lambda x: limited(x["number"], x["lim_edition"]), axis=1
+)
 
 df_1[df_1["lim_edition"] != ""].sample(5).T
 # -
@@ -307,9 +318,12 @@ df_3["rating"] = np.where(df_3["rating"] == 0, 1, df_3["rating"])
 df_3["review"] = ""
 
 df_3["credit_value"] = np.where(
-    (df_3["record_format_id"].isin([3, 4, 5, 7, 8, 9, 10]) & (df_3["is_active"] == True)),
+    (
+        df_3["record_format_id"].isin([3, 4, 5, 7, 8, 9, 10])
+        & (df_3["is_active"] == True)
+    ),
     1,
-    0
+    0,
 )
 
 for col in ["is_active", "is_digitized"]:
@@ -339,12 +353,10 @@ engine_pg
 
 # -
 
+
 def migrate_data(engine_pg, engine_sqlite, table_pg, table_sqlite, cols_to_rename):
     # Read sqlite into df
-    df = pd.read_sql(
-        f"SELECT * FROM {table_sqlite}",
-        engine_sqlite
-    )
+    df = pd.read_sql(f"SELECT * FROM {table_sqlite}", engine_sqlite)
 
     # Replace updated at with actual timestamp
     df["updated_at"] = datetime.now()
@@ -352,69 +364,56 @@ def migrate_data(engine_pg, engine_sqlite, table_pg, table_sqlite, cols_to_renam
 
     # Write into pg_table
     df.to_sql(
-        table_pg, 
-        engine_pg, 
-        index=False, 
-        index_label="id", # NOTE: you have to rename sqlite_index to "id"
-        if_exists="append"
+        table_pg,
+        engine_pg,
+        index=False,
+        index_label="id",  # NOTE: you have to rename sqlite_index to "id"
+        if_exists="append",
     )
 
 
 # ### Write Countries to db
 
 # +
-countries = pd.read_sql(
-    "SELECT * FROM countries",
-    engine
-)
+countries = pd.read_sql("SELECT * FROM countries", engine)
 
 countries["created_at"] = datetime.now()
 countries["updated_at"] = datetime.now()
 
 countries.to_sql(
-    'discobase_country', 
-    engine_pg, 
-    index=False, 
-    index_label="id", 
-    if_exists="append"
+    "discobase_country", engine_pg, index=False, index_label="id", if_exists="append"
 )
 # -
 
 # ### Write Genres to db
 
 migrate_data(
-    engine_pg=engine_pg, 
+    engine_pg=engine_pg,
     engine_sqlite=engine,
-    table_pg="discobase_genre", 
-    table_sqlite="genres", 
-    cols_to_rename={"genre_id": "id"}
+    table_pg="discobase_genre",
+    table_sqlite="genres",
+    cols_to_rename={"genre_id": "id"},
 )
 
 # %sql SELECT * FROM genres LIMIT 2
 
-pd.read_sql(
-    "SELECT * FROM discobase_genre",
-    engine_pg
-)
+pd.read_sql("SELECT * FROM discobase_genre", engine_pg)
 
 # ### Write Formats to db
 
 # +
 
 migrate_data(
-    engine_pg=engine_pg, 
+    engine_pg=engine_pg,
     engine_sqlite=engine,
-    table_pg="discobase_recordformat", 
-    table_sqlite="formats", 
-    cols_to_rename={"format_id": "id"}
+    table_pg="discobase_recordformat",
+    table_sqlite="formats",
+    cols_to_rename={"format_id": "id"},
 )
 
 # -
 
-pd.read_sql(
-    "SELECT * FROM discobase_recordformat",
-    engine_pg
-)
+pd.read_sql("SELECT * FROM discobase_recordformat", engine_pg)
 
 # ### Write Artists to db
 #
@@ -424,10 +423,7 @@ cmap = dict(zip(countries["country_name"], countries["id"]))
 list(cmap.items())[:3]
 
 # +
-artists = pd.read_sql(
-    "SELECT * FROM artists",
-    engine
-)
+artists = pd.read_sql("SELECT * FROM artists", engine)
 
 artists["artist_country"] = artists["artist_country"].map(cmap)
 
@@ -440,32 +436,29 @@ artists = artists.rename(columns={"artist_country": "country_id", "artist_id": "
 
 # Write into pg_table
 artists.to_sql(
-    "discobase_artist", 
-    engine_pg, 
-    index=False, 
-    index_label="id", # NOTE: you have to rename sqlite_index to "id"
-    if_exists="append"
+    "discobase_artist",
+    engine_pg,
+    index=False,
+    index_label="id",  # NOTE: you have to rename sqlite_index to "id"
+    if_exists="append",
 )
 # -
 
 # ### Write Labels to db
 
 migrate_data(
-    engine_pg=engine_pg, 
+    engine_pg=engine_pg,
     engine_sqlite=engine,
-    table_pg="discobase_label", 
-    table_sqlite="labels", 
-    cols_to_rename={"label_id": "id"}
+    table_pg="discobase_label",
+    table_sqlite="labels",
+    cols_to_rename={"label_id": "id"},
 )
 
 # %sql SELECT * FROM labels
 
 # ### Write Records to db
 
-pd.read_sql(
-    "SELECT * FROM discobase_record",
-    engine_pg
-)
+pd.read_sql("SELECT * FROM discobase_record", engine_pg)
 
 df_3.head(2).T
 
@@ -476,11 +469,11 @@ df_3 = df_3.rename(columns={"record_id": "id"})
 
 # Write into pg_table
 df_3.to_sql(
-    "discobase_record", 
-    engine_pg, 
-    index=False, 
-    index_label="id", # NOTE: you have to rename sqlite_index to "id"
-    if_exists="append"
+    "discobase_record",
+    engine_pg,
+    index=False,
+    index_label="id",  # NOTE: you have to rename sqlite_index to "id"
+    if_exists="append",
 )
 
 # +
@@ -494,65 +487,59 @@ df_3.iloc[314]
 # ### Write TrxCredit to db
 
 migrate_data(
-    engine_pg=engine_pg, 
+    engine_pg=engine_pg,
     engine_sqlite=engine,
-    table_pg="discobase_trxcredit", 
-    table_sqlite="credit_trx", 
-    cols_to_rename={"credit_trx_id": "id"}
+    table_pg="discobase_trxcredit",
+    table_sqlite="credit_trx",
+    cols_to_rename={"credit_trx_id": "id"},
 )
 
-pd.read_sql(
-    "SELECT * FROM discobase_trxcredit",
-    engine_pg
-)
+pd.read_sql("SELECT * FROM discobase_trxcredit", engine_pg)
 
 # %sql SELECT * FROM credit_trx LIMIT 5
 
 # ### Write links to db
 
-pd.read_sql(
-    "SELECT * FROM discobase_record_artists",
-    engine_pg
-)
+pd.read_sql("SELECT * FROM discobase_record_artists", engine_pg)
 
 
 # %sql SELECT * FROM artist_record_link LIMIT 5
 
-def migrate_data_without_id(engine_pg, engine_sqlite, table_pg, table_sqlite, cols_to_rename):
+
+def migrate_data_without_id(
+    engine_pg, engine_sqlite, table_pg, table_sqlite, cols_to_rename
+):
     # Read sqlite into df
-    df = pd.read_sql(
-        f"SELECT * FROM {table_sqlite}",
-        engine_sqlite
-    )
+    df = pd.read_sql(f"SELECT * FROM {table_sqlite}", engine_sqlite)
     df = df.reset_index()
     df["index"] = df["index"] + 1
-    
+
     # Replace updated at with actual timestamp
     df = df.rename(columns=cols_to_rename)
 
     # Write into pg_table
     df.to_sql(
-        table_pg, 
-        engine_pg, 
-        index=False, 
-        index_label="id", # NOTE: you have to rename sqlite_index to "id"
-        if_exists="append"
+        table_pg,
+        engine_pg,
+        index=False,
+        index_label="id",  # NOTE: you have to rename sqlite_index to "id"
+        if_exists="append",
     )
 
 
 migrate_data_without_id(
-    engine_pg=engine_pg, 
+    engine_pg=engine_pg,
     engine_sqlite=engine,
-    table_pg="discobase_record_artists", 
-    table_sqlite="artist_record_link", 
-    cols_to_rename={"index": "id"}
+    table_pg="discobase_record_artists",
+    table_sqlite="artist_record_link",
+    cols_to_rename={"index": "id"},
 )
 
 # + active=""
 # migrate_data_without_id(
-#     engine_pg=engine_pg, 
+#     engine_pg=engine_pg,
 #     engine_sqlite=engine,
-#     table_pg="discobase_record_labels", 
-#     table_sqlite="record_label_link", 
+#     table_pg="discobase_record_labels",
+#     table_sqlite="record_label_link",
 #     cols_to_rename={"index": "id"}
 # )
